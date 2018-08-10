f221f757b5da8d63f8bd8c7f02250a199b35e51a
Ahmed Omar <Ahmed@Saras-MacBook-Air.local>
2018-08-10 16:55:49 -0500

    Created controller and app as well as established some of checklist in README - created variables required for the server.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # weekendChallenge_3
+
+[ ] Source Angular.js and test functionality
+[ ] Create ng-app
+[ ] Create ng-controller
+[ ] Create input-model
+[ ] create submit button 
+[ ] post to the server  
+[ ]
+[ ]
---
+++ server/server.js
@@ -0,0 +1,8 @@
+//REQUIRED VARIABLES FOR OPTIMAL SERVER FUNCTIONALITY 
+const express = require( 'express' );
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // AngularJS
+
+
