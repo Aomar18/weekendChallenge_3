0b451f550a47c15d81d4bd660e80a372f6507320
Ahmed Omar <Ahmed@Saras-MacBook-Air.local>
2018-08-12 18:01:12 -0500

    created controller and initialized app

--- server/scripts/client.js
+++ server/scripts/client.js
@@ -1,1 +1,13 @@
 console.log('js');
+
+
+//INITIALIZE APP 
+const toDoApp =  angular.module('toDoController' , [] );
+
+//INITIALIZE CONTROLLER
+toDoApp.controller('toDoController' , function($http){
+console.log('toDoController set up')
+const tdc = this;
+tdc.tasks = [];
+});
+
