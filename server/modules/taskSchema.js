73d4b7671d58a1ce57e0a7fb8730aeea56f48ef5
Ahmed Omar <Ahmed@Saras-MacBook-Air.local>
2018-08-13 07:03:28 -0500

    Styling

--- server/modules/taskSchema.js
+++ server/modules/taskSchema.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TasklistSchema = new Schema({
-    taskIn: { type: String } ,
-    taskDone: {type: Boolean}
+    taskIn: { type: String } 
 });  
 
 module.exports = mongoose.model('taskList', TasklistSchema);
